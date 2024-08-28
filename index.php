a20e2b4f5308c3b09702b18b8b716db6754640bf
Alysa-Gathoni <alysa.macharia@strathmore.edu>
2024-08-28 15:46:11 +0300

    second commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport">
+        <title>Sem</title>
+    </head>
+    <body>
+        <h1>This is going to be a wonderful semester</h1>
+        <?php
+        require_once "load.php";
+        print $Obj->user_age("Alysa", 2004)
+        ?>
+    </body>
+    
+</html>
