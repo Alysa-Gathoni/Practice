27ebf2edb18761f7d54ac3bcc39791afec8506e9
Alysa-Gathoni <alysa.macharia@gmail.com>
2024-10-01 23:19:25 +0300

    menu update four

--- menus/menus.php
+++ menus/menus.php
@@ -66,4 +66,30 @@ class menus{
                 <span class="navbar-toggler-icon"></span>
               </button>
 
-              
+              <div class="collapse navbar-collapse" id="navbarsExample07XL">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="./">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Our Projects</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="signup.php">Sign Up</a>
+                  </li>
+                </ul>
+                <form role="search">
+                  <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                </form>
+              </div>
+            </div>
+          </nav>
+      </header>
+        <?php
+    }
